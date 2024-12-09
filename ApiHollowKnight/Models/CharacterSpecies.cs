a10effa56e8cff65b9798f515a019bf30c5aa2cc
Arthur Bomfim <arthurbao10@gmail.com>
2024-12-09 08:20:29 -0300

    Craiação da classe CharacterSpecies

--- ApiHollowKnight/Models/CharacterSpecies.cs
+++ ApiHollowKnight/Models/CharacterSpecies.cs
@@ -1,6 +1,20 @@
-﻿namespace ApiHollowKnight.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiHollowKnight.Models
 {
+    [Table("CharacterSpecies")]
     public class CharacterSpecies
     {
+        [Key]
+        public int CharactersSpeciesId { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string CharactersSpeciesName { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        public string CharactersSpeciesDescription { get; set; }
     }
 }
