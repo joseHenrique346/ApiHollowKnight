c1fcc383c456c774a1a4193a792b4870779fb166
José Henrique <josehenriquefernandes346@gmail.com>
2024-12-09 09:52:55 -0300

    Criacao do Repository e Controlles PLaces

---
+++ ApiHollowKnight/Controllers/PlacesController.cs
@@ -0,0 +1,50 @@
+﻿using ApiHollowKnight.Models;
+using ApiHollowKnight.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiHollowKnight.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PlacesController : ControllerBase
+    {
+        private readonly IPlacesRepository _repository;
+
+        public PlacesController(IPlacesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<Place> GetPlaces()
+        {
+            var character = _repository.GetPlaces().ToList();
+            return Ok(character);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Place> GetPlace(int id)
+        {
+            var character = _repository.GetPlace(id);
+            return Ok(character);
+        }
+
+        [HttpPost]
+        public ActionResult<Place> Create(Place place)
+        {
+            return Ok(_repository.Create(place));
+        }
+
+        [HttpPut]
+        public ActionResult<Place> Update(Place place)
+        {
+            return Ok(_repository.Update(place));
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            return Ok(_repository.Delete(id));
+        }
+    }
+}
---
+++ ApiHollowKnight/Repositories/IPlacesRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApiHollowKnight.Models;
+
+namespace ApiHollowKnight.Repositories
+{
+    public interface IPlacesRepository
+    {
+        IEnumerable<Place> GetPlaces();
+        Place GetPlace(int id);
+        Place Create(Place place);
+        Place Update(Place place);
+        Place Delete(int id);
+    }
+}
