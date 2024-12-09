bc59886ddce8b2ecdb8c56bc832075bdf3eec0f3
José Henrique <josehenriquefernandes346@gmail.com>
2024-12-09 08:21:00 -0300

    Criação da Interface CharacterSpecies

---
+++ ApiHollowKnight/Repositories/ICharacterSpeciesRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApiHollowKnight.Models;
+
+namespace ApiHollowKnight.Repositories
+{
+    public interface ICharacterSpeciesRepository
+    {
+        IEnumerable<CharacterSpecies> GetCharacterSpecies();
+        CharacterSpecies GetCharacterSpeciesById(int id);
+        CharacterSpecies Create(CharacterSpecies character);
+        CharacterSpecies Update(CharacterSpecies character);
+        CharacterSpecies Delete(int id);
+    }
+}
