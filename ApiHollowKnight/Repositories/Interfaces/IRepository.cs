88c743e40b2c413bb4177bb234e99afd9fe68b5f
José Henrique <josehenriquefernandes346@gmail.com>
2024-12-11 16:29:21 -0300

    Comentando tasks

--- ApiHollowKnight/Repositories/Interfaces/IRepository.cs
+++ ApiHollowKnight/Repositories/Interfaces/IRepository.cs
@@ -4,8 +4,10 @@ namespace ApiHollowKnight.Repositories.Interfaces
 {
     public interface IRepository<T>
     {
-        Task<List<T>> GetAllAsync();
-        Task<T?> GetAsync(int id);
+        //Task<List<T>> GetAllAsync();
+        //Task<T?> GetAsync(int id);
+        List<T> GetAll();
+        T? Get(int id);
         T Create(T entity);
         T Update(T entity);
         bool Delete(T entity);
