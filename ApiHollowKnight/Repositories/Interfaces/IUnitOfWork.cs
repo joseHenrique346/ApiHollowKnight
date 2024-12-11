c148e50fe9fd321ae3eb9fb92d5882ffce2ced02
José Henrique <josehenriquefernandes346@gmail.com>
2024-12-11 12:36:20 -0300

    Criação do UnitOfWork

---
+++ ApiHollowKnight/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using ApiHollowKnight.Models;
+using ApiHollowKnight.Repositories.Interfaces;
+
+namespace ApiHollowKnight.Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICharactersRepository CharacterRepository { get; }
+        ICharacterSpeciesRepository CharacterSpeciesRepository { get; }
+        ICharacterTypeRepository CharacterTypeRepository { get; }
+        IPlacesRepository PlacesRepository { get; }
+        //IRepository<Character> CharacterRepository { get; }
+        //IRepository<CharacterSpecies> CharacterSpeciesRepository { get; }
+        //IRepository<CharacterType> CharacterTypeRepository { get; }
+        //IRepository<Place> PlaceRepository { get; }
+        void Commit();
+    }
+}
---
+++ ApiHollowKnight/Repositories/Repositories/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using ApiHollowKnight.Repositories.Interfaces;
+
+namespace ApiHollowKnight.Repositories.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ICharactersRepository _characterRepo;
+
+        private ICharacterSpeciesRepository _characterSpeciesRepo;
+
+        private ICharacterTypeRepository _characterTypeRepo;
+
+        private IPlacesRepository _placesRepo;
+        public AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICharactersRepository CharacterRepository
+        {
+            get 
+            { 
+                return _characterRepo = _characterRepo ?? new CharacterRepository(_context); 
+            }
+        }
+
+        public ICharacterSpeciesRepository CharacterSpeciesRepository
+        {
+            get
+            {
+                return _characterSpeciesRepo = _characterSpeciesRepo ?? new CharacterSpeciesRepository(_context);
+            }
+        }
+
+        public ICharacterTypeRepository CharacterTypeRepository
+        {
+            get
+            {
+                return _characterTypeRepo = _characterTypeRepo ?? new CharacterTypeRepository(_context);
+            }
+        }
+
+        public IPlacesRepository PlacesRepository
+        {
+            get
+            {
+                return _placesRepo = _placesRepo ?? new PlacesRepository(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
